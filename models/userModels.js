c6d628d4252d6001546d3cff5f43db68ca3f700a
rishabht123 <rishabht1@yahoo.co.in>
2020-08-08 16:35:11 +0530

    Final

--- models/userModels.js
+++ models/userModels.js
@@ -51,17 +51,17 @@ const userSchema = new mongoose.Schema(
       minlength: 8,
       select: false,
     },
-    passwordConfirm: {
-      type: String,
-      required: [true, 'Please confirm your password'],
-      validate: {
-        //  This only works on create and save
-        validator: function (el) {
-          return el === this.password;
-        },
-        message: 'Passwords are not the same',
-      },
-    },
+    // passwordConfirm: {
+    //   type: String,
+    //   required: [true, 'Please confirm your password'],
+    //   validate: {
+    //     //  This only works on create and save
+    //     validator: function (el) {
+    //       return el === this.password;
+    //     },
+    //     message: 'Passwords are not the same',
+    //   },
+    // },
     // salt: String,
     role: {
       type: String,
